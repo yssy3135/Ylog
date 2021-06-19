e6eb0229cc1ef1bd699ec96f01eb7aed6a0a24f6
ys3135 <ys3135@gmail.com>
2021-06-19 23:54:46 +0900

    복구

---
+++ server/index.ts
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const config = require("./config/key");
+
+
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+    {
+        useNewUrlParser: true, useUnifiedTopology: true,
+        useCreateIndex: true, useFindAndModify: false
+
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch( (err: Error) => console.log(err));
+
+
+app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use('/api/users', require('./routes/users'));
+app.use('/api/contents',require('./routes/contents'))
+app.use('/api/categorys',require('./routes/categorys'))
+
+app.use('/uploads', express.static('uploads'));
+
+
+
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
+});
